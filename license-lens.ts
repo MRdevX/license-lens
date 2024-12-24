8b3288293be5692f4928559cee30fd65ab1f4b4f
Mahdi Rashidi <m8rashidi@gmail.com>
2024-12-24 16:44:10 +0300

    chore: remove outdated comments from license-lens.ts

--- license-lens.ts
+++ license-lens.ts
@@ -1,12 +1,3 @@
-/**
- * Main entry point for the License Scout tool.
- *
- * This module exports all necessary interfaces, services, and utilities for
- * scanning and analyzing license information in projects.
- *
- * @module
- */
-
 export * from "./src/interfaces/index.ts";
 export * from "./src/services/index.ts";
 export * from "./src/utils/index.ts";
